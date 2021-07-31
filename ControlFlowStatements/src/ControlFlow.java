b1cd34556d6473eae8a2b5315de35f6075df1e7f
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2021-07-31 14:50:22 +0530

    updated Control flow added

--- ControlFlowStatements/src/ControlFlow.java
+++ ControlFlowStatements/src/ControlFlow.java
@@ -4,8 +4,29 @@ public class ControlFlow {
         System.out.println("10000 at 3% interest = "+calculateInterest(10000,3.0));
         System.out.println("10000 at 4% interest = "+calculateInterest(10000,4.0));
         System.out.println("10000 at 5% interest = "+calculateInterest(10000,5.0));
+
+
     }
     public static double calculateInterest(double amount, double interestRate){
         return (amount*(interestRate/100));
     }
+
+        public static boolean isOdd(int parameter){
+            if (parameter>0){
+                return (parameter % 2 != 0) || (parameter == 2);
+            } else return false;
+        }
+
+        public static int sumOdd(int start, int end){
+            if (end < start || start < 0){
+                return -1;
+            }
+            int sum = 0;
+            for (int i = start; i<=end;i++){
+                if (isOdd(i)){
+                    sum +=i;
+                }
+            }
+            return sum;
+        }
 }
