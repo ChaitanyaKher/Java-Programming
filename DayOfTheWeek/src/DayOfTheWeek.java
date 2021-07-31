749593264cd9413dac61d5e875252ce3d03391ca
Kher <ckkher@amazon.com>
2021-07-31 12:59:45 +0530

    Adding a wrong code

--- DayOfTheWeek/src/DayOfTheWeek.java
+++ DayOfTheWeek/src/DayOfTheWeek.java
@@ -1,76 +1,137 @@
 public class DayOfTheWeek {
-        private static void printDayOfTheWeek (int day){
-            switch (day){
-                case 0:
-                    System.out.println("Sunday");
-                    break;
-                case 1:
-                    System.out.println("Monday");
-                    break;
-                case 2:
-                    System.out.println("Tuesday");
-                    break;
-                case 3:
-                    System.out.println("Wednesday");
-                    break;
-                case 4:
-                    System.out.println("Thursday");
-                    break;
-                case 5:
-                    System.out.println("Friday");
-                    break;
-                case 6:
-                    System.out.println("Saturday");
-                    break;
-                default:
-                    System.out.println("Invalid Day");
-            }
-    }
-
-    public static void printNumberInWord(int number){
-            switch (number){
-                case 0:
-                    System.out.println("ZERO");
-                    break;
-                case 1:
-                    System.out.println("ONE");
-                    break;
-                case 2:
-                    System.out.println("TWO");
-                    break;
-                case 3:
-                    System.out.println("THREE");
-                    break;
-                case 4:
-                    System.out.println("FOUR");
-                    break;
-                case 5:
-                    System.out.println("FIVE");
-                    break;
-                case 6:
-                    System.out.println("SIX");
-                    break;
-                case 7:
-                    System.out.println("SEVEN");
-                    break;
-                case 8:
-                    System.out.println("EIGHT");
-                    break;
-                case 9:
-                    System.out.println("NINE");
-                    break;
-            }
-    }
-
-    public static boolean isLeapYear(int year){
-            if (year>1&&year<9999){
-
-            }else return false;
-    }
-
-    public static void main(String[] args) {
-        for (int i=0;i<7;i++){
-            DayOfTheWeek.printDayOfTheWeek(i);
+    private static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid Day");
         }
+    }
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+        }
+    }
+    public static class NumberOfDaysInAMonth{
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 != 0) {
+                return false;
+            } else if (year % 400 == 0) {
+                return true;
+            } else if (year % 100 == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if ((month >= 1 || month <= 12) && (year > 1 || year < 9999)) {
+            boolean LeapYearCheck = NumberOfDaysInAMonth.isLeapYear(year);
+            int TotalDays;
+            if (LeapYearCheck) {
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 9:
+                    case 11:
+                        TotalDays = 31;
+                        return TotalDays;
+                    case 2:
+                        TotalDays = 29;
+                        return TotalDays;
+                    case 4:
+                    case 6:
+                    case 8:
+                    case 10:
+                    case 12:
+                        TotalDays = 30;
+                        return TotalDays;
+                    default:
+                        return -1;
+                }
+            } else {
+                switch (month) {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 9:
+                    case 11:
+                        TotalDays = 31;
+                        return TotalDays;
+                    case 2:
+                        TotalDays = 28;
+                        return TotalDays;
+                    case 4:
+                    case 6:
+                    case 8:
+                    case 10:
+                        TotalDays = 30;
+                        return TotalDays;
+                    default:
+                        return -1;
+                }
+            }
+        } else return -1;
+    }
+}
+    public static void main(String[] args) {
+        System.out.println(NumberOfDaysInAMonth.getDaysInMonth(1,2020));
     }
 }
