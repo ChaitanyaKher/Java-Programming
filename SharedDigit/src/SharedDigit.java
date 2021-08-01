cf41b8fc70b866e9b534e84aca630f1983fdbffe
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2021-08-01 11:54:23 +0530

    added even digit sum

---
+++ SharedDigit/src/SharedDigit.java
@@ -0,0 +1,19 @@
+public class SharedDigit {
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(91,93));
+    }
+
+    public static boolean hasSharedDigit(int firstNumber, int secondNumber) {
+        if ((firstNumber >= 10 && firstNumber <= 99) && (secondNumber >= 10 && secondNumber <= 99)) {
+            int firstNumberLastDigit = firstNumber % 10;
+            int secondNumberLastDigit = secondNumber % 10;
+            firstNumber /= 10;
+            secondNumber /= 10;
+            int firstNumberFirstDigit = firstNumber;
+            int secondNumberFirstDigit = secondNumber;
+            return ((firstNumberFirstDigit == secondNumberFirstDigit) || (firstNumberFirstDigit == secondNumberLastDigit) || (firstNumberLastDigit == secondNumberFirstDigit) || (firstNumberLastDigit == secondNumberLastDigit));
+        }
+        System.out.println("Invalid input.");
+        return false;
+    }
+}
