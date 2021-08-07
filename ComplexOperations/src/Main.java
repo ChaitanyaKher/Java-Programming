305d5f979d4f6e8eef22e4c1b51f1900a3fcbc0f
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2021-08-07 20:45:52 +0530

    Complex Operation

---
+++ ComplexOperations/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ComplexNumber one = new ComplexNumber(1.0,1.0);
+        ComplexNumber number = new ComplexNumber(2.5,-1.5);
+        one.add(1,1);
+        System.out.println("one.real");
+    }
+}
