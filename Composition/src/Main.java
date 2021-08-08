b3a426ed98e17d87d6e53d13e964a9c77ba3cb7c
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2021-08-08 22:54:33 +0530

    House Design using composition and inheritance

---
+++ Composition/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B","Dell","240",dimensions);
+        Monitor theMonitor = new Monitor("Gaming Rig","Alienware",27,new Resolution(2540,1440));
+
+        Motherboard motherboard = new Motherboard("BJ-200","Intel",4,6,"v2.4.4");
+        PC thePC = new PC(theCase,theMonitor,motherboard);
+        thePC.powerUp();
+    }
+}
