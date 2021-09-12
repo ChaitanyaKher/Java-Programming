9f4859bb69cb503a34ef0e41f53686245eafaf45
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2021-09-12 17:40:14 +0530

    Applied Encapsulation

--- Encapsulation/src/Main.java
+++ Encapsulation/src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player();
-        player.name="Chaitanya";
-        player.health = 100;
-        player.weapon = "Katana";
+//        Player player = new Player();
+//        player.name="Chaitanya";
+//        player.health = 100;
+//        player.weapon = "Katana";
+//
+//        int damage =25;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health = "+player.healthRemaining());
+//
+//        damage =101;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining Health = "+player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("Rohan", 100, "Cross bow");
+        System.out.println("Initial health is " + player.getHealth());
 
-        int damage =25;
-        player.loseHealth(damage);
-        System.out.println("Remaining Health = "+player.healthRemaining());
     }
 }
